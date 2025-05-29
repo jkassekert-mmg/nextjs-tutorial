a4d74bb37871267f50db62e11d6a88a893917817
jkassekert-mmg <jkassekert@mmgapts.com>
2025-05-29 16:39:53 -0400

    added hello world page and url

---
+++ app/helloWorld/page.tsx
@@ -0,0 +1,8 @@
+export default function Hello(){
+    return(
+        <div className="text-center text-3xl font-bold underline">
+            <h1>Hello, Next.js</h1>
+            <h2>Welcome</h2>
+        </div>
+    ) 
+}
